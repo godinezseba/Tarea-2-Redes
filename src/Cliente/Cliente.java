2108a3c4d3781557464e6b966027afb8f7293332
Sebastian Godinez <sebastian.godinez@sansano.usm.cl>
2019-05-16 17:46:59 -0400

    cambie ip del server en cliente

--- src/Cliente/Cliente.java
+++ src/Cliente/Cliente.java
@@ -14,7 +14,7 @@ import java.net.Socket;
 public class Cliente {
     public static void main(String[] args) throws IOException {
         String mensaje, mensajeterminal;
-        Socket socket = new Socket("10.6.40.172", 1234);
+        Socket socket = new Socket("127.0.0.1", 1234);
         Scanner inputterminal;
         
 
